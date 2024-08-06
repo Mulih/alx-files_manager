6bd3e9fa5a619d33b057a28c2faa8274c296e005
Mulih <almulih@yahoo.com>
2024-08-06 11:49:28 +0300

    fix parsing error

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
 const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
-const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+const url = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
 /**
  * Class for performing operations with Mongo service
@@ -22,22 +22,29 @@ class DBClient {
       }
     });
   }
-
+  
   /**
    * It checks if the connection to Redis is Alive.
    * @return true if connection is alive of false if not.
    */
+  isAlive() {
+    return Boolean(this.db);
+  }
 
+  /**
+   * Retrieves the number of users in the database.
+   * @returns {Promise<Number>}
+   */
   async nbUsers() {
     const numberOfUsers = this.usersCollection.countDocuments();
-    return numberOfUsers;
+    return numberOfUsers
   }
 
   /**
    * Returns the number of files
    */
   async nbFiles() {
-    const numberOfFiles = this.filesCollection.countDocuments();
+    const numberOfFiles = this.usersCollection.countDocuments();
     return numberOfFiles;
   }
 }
